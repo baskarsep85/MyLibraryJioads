c0faedeca41d109570897473222586e582b5cc49
Prasad Kandukuri <prasad.kandukuri@ril.com>
2022-12-20 14:58:55 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MyLibraryJioads
+
+A description of this package.
---
+++ Sources/MyLibraryJioads/MyLibraryJioads.swift
@@ -0,0 +1,6 @@
+public struct MyLibraryJioads {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
