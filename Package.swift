9c81d28e65166bca30689071d09013a5da963616
Prasad Kandukuri <prasad.kandukuri@ril.com>
2022-12-20 15:25:12 +0530

    Test

--- Package.swift
+++ Package.swift
@@ -5,11 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "MyLibraryJioads",
+    platforms: [.tvOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+        
         .library(
             name: "MyLibraryJioads",
-            targets: ["MyLibraryJioads"]),
+            targets: ["JioAdsFrameworkTVOS"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -18,11 +20,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MyLibraryJioads",
-            dependencies: []),
-        .testTarget(
-            name: "MyLibraryJioadsTests",
-            dependencies: ["MyLibraryJioads"]),
+        .binaryTarget(name: "JioAdsFrameworkTVOS", path: "./Sources/JioAdsFrameworkTVOS.xcframework"),
     ]
 )
--- Sources/MyLibraryJioads/MyLibraryJioads.swift
+++
@@ -1,6 +0,0 @@
-public struct MyLibraryJioads {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
